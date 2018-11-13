b6236be1495992c6d256027bbbe4b85444a0696f
southboundshawty <44846584+southboundshawty@users.noreply.github.com>
2018-11-13 09:51:21 +0300

    Add files via upload

--- script/media.js
+++ script/media.js
@@ -1,19 +1,12 @@
 $(window).ready(function() {
-    if($(window).width() <= "480") {
-        // $("nav").addClass(" nav-mini");
-        // $(".nav-container").addClass(" nav-container-mini");
-    }
+  if ($(window).width() <= "480") {
 
-    $(".search").click(function() {
-        $(".nav-mini").toggleClass("nav-mini-opened");
-        $(".nav-container").toggleClass("nav-container-mini-opened");
-        $(".nav__item-mini").toggleClass(".nav__item-mini"); 
-        $("#search-mini").toggleClass("#search-mini-opened");
-        $(".hot-news-wrapper").toggleClass("hot-news-wrapper-opened"); 
-        $(".nav-middle").toggleClass("nav-middle-opened");
-    });
+  }
 
-    $(".menuID").click(function() {
-        $(".menu").slideToggle(150);
-    });
-})
+  $("#search-mini").click(function() {
+    $(".nav-mini").toggleClass("nav-mini-opened");
+    $(".nav-mini-content").toggleClass("nav-mini-content-opened");
+    $(".nav-mini-top").toggleClass("nav-mini-top-opened");
+    $(".nav-mini-footer").toggleClass("nav-mini-footer-opened");
+  });
+});
